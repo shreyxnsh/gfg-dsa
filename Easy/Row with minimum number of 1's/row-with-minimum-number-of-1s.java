742537486e137b47c113e4305cf05390fdab70ee
shreyxnsh <88729972+shreyxnsh@users.noreply.github.com>
2024-04-22 21:45:33 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Row with minimum number of 1's/row-with-minimum-number-of-1s.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s[] = read.readLine().split(" ");
+            int N = Integer.parseInt(s[0]);
+            int M = Integer.parseInt(s[1]);
+            int A[][] = new int[N][M];
+            for (int i = 0; i < N; i++) {
+                String S[] = read.readLine().split(" ");
+                for (int j = 0; j < M; j++) {
+                    A[i][j] = Integer.parseInt(S[j]);
+                }
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.minRow(N, M, A));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+
+
+// User function Template for Java
+
+class Solution {
+    int minRow(int n, int m, int a[][]) {
+        // code here
+        int ans=m+1;
+        int ind=-1;
+        for(int i=0; i<n; i++)
+        {
+            int cnt=0;
+            for(int j=0; j<m; j++)
+            {
+                if(a[i][j]==1)
+                cnt++;
+            }
+            if(cnt<ans){
+                ind=i+1;
+                ans=cnt;
+            }
+        }
+        return ind;
+    }
+};
